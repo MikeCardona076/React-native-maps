dff45533c8d5930e84b099fbb4e60cb9e1731dd7
mikecardona076 <mikecardona076@gmail.com.com>
2021-04-21 14:42:18 -0700

    First commit

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image  } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <NavigationContainer>
+      
+      <Image
+        source={{uri: "https://reactnative.dev/docs/assets/p_cat1.png"}}
+        style={{width: 200, height: 200}}
+      />
+      
+
+      <Stack.Navigator>
+
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Mapas con Mike el depredador' }}
+        />
+
+        <Stack.Screen name="Mapas" component={ProfileScreen} />
+
+      </Stack.Navigator>
+
+
+    </NavigationContainer>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <Button
+      title="Comenzar"
+      onPress={() =>
+        navigation.navigate('Mapas', { name: 'Jane' })
+      }
+    />
+  );
+};
+
+
+const ProfileScreen = ({ navigation, route }) => {
+  return <Text>Mapas con Mike</Text>;
+};
+
+
+
+
