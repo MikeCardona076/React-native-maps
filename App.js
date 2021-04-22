61c097948a4d87a7ddfb94e2c856f137b1a20611
mikecardona076 <mikecardona076@gmail.com.com>
2021-04-21 21:28:13 -0700

    Movies Done

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Button, Image  } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button, Image,TextInput, ActivityIndicator, FlatList} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -10,24 +10,19 @@ const Stack = createStackNavigator();
 
 export default function App() {
   return (
+    
 
     <NavigationContainer>
-      
-      <Image
-        source={{uri: "https://reactnative.dev/docs/assets/p_cat1.png"}}
-        style={{width: 200, height: 200}}
-      />
-      
 
       <Stack.Navigator>
 
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{ title: 'Mapas con Mike el depredador' }}
+          options={{ title: 'Mike Movies' }}
         />
 
-        <Stack.Screen name="Mapas" component={ProfileScreen} />
+        <Stack.Screen name="Movies" component={ProfileScreen} />
 
       </Stack.Navigator>
 
@@ -39,20 +34,69 @@ export default function App() {
 
 
 const HomeScreen = ({ navigation }) => {
+
   return (
-    <Button
-      title="Comenzar"
-      onPress={() =>
-        navigation.navigate('Mapas', { name: 'Jane' })
-      }
-    />
+      <View style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
+
+          <Image
+            source={{uri: "https://c0.klipartz.com/pngpicture/241/690/gratis-png-pelicula-grafica-iconos-computacionales-cine-dvd.png"}}
+            style={{width: 200, height: 200}}
+          />
+
+        <Button
+            title="Comenzar"
+            onPress={() =>
+                navigation.navigate('Movies', { name: 'Mike' })
+              }
+        />
+            
+    </View>
+
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+  },
+});
+
+const ProfileScreen = ({ navigation, route }) => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://reactnative.dev/movies.json')
+      .then((response) => response.json())
+      .then((json) => setData(json.movies))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+
+  return (
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>{item.title}, {item.releaseYear}</Text>
+          )}
+        />
+      )}
+    </View>
   );
 };
 
 
-const ProfileScreen = ({ navigation, route }) => {
-  return <Text>Mapas con Mike</Text>;
-};
+
 
 
 
